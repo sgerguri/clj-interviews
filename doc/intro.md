584007d30eccb610e1dbbb772e12c2c61ea96f9a
Shkodran Gerguri <shkodran.gerguri@actual-experience.com>
2016-10-19 11:33:18 +0100

    add project skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-interviews
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_interviews/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-interviews.core-test
+  (:require [clojure.test :refer :all]
+            [clj-interviews.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
